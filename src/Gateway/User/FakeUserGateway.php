7b534369319a114311bccc2baa45d08718418377
Clemente <bruno.canguru@hotmail.com>
2024-02-29 14:37:45 -0300

    Finish fake users gateway

--- src/Gateway/User/FakeUserGateway.php
+++ src/Gateway/User/FakeUserGateway.php
@@ -2,8 +2,22 @@
 
 namespace Clemento\Gateway\User;
 
+use Cake\Chronos\Chronos;
+use Clemento\Domain\User\Entities\UserName;
 use Clemento\Domain\User\Gateways\UserGateway;
+use Clemento\Domain\User\Entities\UserId;
+use Clemento\Domain\User\Entities\User;
+use Clemento\Domain\User\Entities\UserRole;
 
 class FakeUserGateway implements UserGateway {
 
+    public function getUser(UserId $user_id): User {
+        return new User(
+            $user_id,
+            new UserName("Bruno Clemente"),
+            [UserRole::Editor],
+            Chronos::now(),
+            Chronos::now(),
+        );
+    }
 }
